3b082476a335f5ccece2375622c1804bda351903
Dustin Woodard <dustinwoodard@weber.edu>
2014-01-02 19:03:53 -0700

    transcode.php almost there

--- app/queue/Transcode.php
+++ app/queue/Transcode.php
@@ -17,46 +17,51 @@ class Transcode {
         $ofp = "$mediaPathOriginal/$originalFilename";
 
 
-        $asset->status = "transcoded:start";
-        $asset->save();
 
         File::append(storage_path() . '/logs/queue.txt', $asset->id . '----' . $ofp .'----' . "$mediaPath/$filenameThumb" .'----' . "$mediaPath/$filename.mp4" . PHP_EOL);
 
-        switch($asset->original_ext){
-            case "mov":
-                exec("sudo ffmpeg -i $ofp -ss 5 $mediaPath/$filenameThumb", $out, $return);
-                exec("sudo ffmpeg -i $ofp -vcodec copy -acodec aac -strict experimental -ac 2 -ar 44100 -ab 192k $mediaPath/$filename.mp4", $out, $return);
-            break;
 
-            case "flv":
+
+
+
+        $asset->status = "transcoded:start";
+        $asset->save();
+
+        switch($asset->type)
+        {
+            case 'video':
+
                 exec("sudo ffmpeg -i $ofp -ss 5 $mediaPath/$filenameThumb", $out, $return);
-                exec("sudo ffmpeg -i $ofp -ar 44100 -ab 192k $mediaPath/$filename.mp4", $out, $return);
-            break;
+
+                 switch($asset->original_ext){
+                    case "mov":
+                        exec("sudo ffmpeg -i $ofp -vcodec copy -acodec aac -strict experimental -ac 2 -ar 44100 -ab 192k $mediaPath/$filename.mp4", $out, $return);
+                    break;
+
+                    case "flv":
+                        exec("sudo ffmpeg -i $ofp -ar 44100 -ab 192k $mediaPath/$filename.mp4", $out, $return);
+                    break;
+
+                    case default:
+                        exec("sudo ffmpeg -i $ofp $mediaPath/$filename.mp4", $out, $return);
+                    break;
+                }
+
+
+
+                $asset->filepath = $transcode_dir;
+                 
+                break;
+
+            case 'audio':
+                exec("sudo ffmpeg -i $ofp $mediaPathOriginal/$filename.mp3", $out, $return );
+                 
+                break;
         }
+
         $asset->status = "transcoded:complete";
         $asset->save();
 
-
-
-        // switch($asset->type)
-        // {
-        //     case 'video':
-
-        //         exec("sudo ffmpeg -i $mediaPathOriginal/$originalFilename -ss 5 $mediaPath/$filenameThumb", $out, $return);
-
-        //         exec("sudo ffmpeg -i $mediaPathOriginal/$originalFilename $mediaPath/$filename.mp4", $out, $return);
-
-        //         $asset->filepath = $transcode_dir;
-        //         $asset->status = "transcoded:complete";
-        //         $asset->save();
-        //         break;
-
-        //     case 'audio':
-        //         exec("sudo ffmpeg -i $mediaPathOriginal/$originalFilename $mediaPathOriginal/$filename.mp3", $out, $return );
-        //         $asset->status = "transcoded:complete";
-        //         $asset->save();
-        //         break;
-        // }
 
         $job->delete();
 
