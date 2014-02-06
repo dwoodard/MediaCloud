67bb62c43be979b349a6e3b9a7d3f6216effc89d
Dustin Woodard <dustinwoodard@weber.edu>
2014-02-06 15:46:27 -0700

    AssetController rangedownload clean up

--- app/controllers/AssetsController.php
+++ app/controllers/AssetsController.php
@@ -215,142 +215,29 @@ class AssetsController extends PermissionsController{
 			return Redirect::to('admin/assets')->with('success', Lang::get('admin/assets/message.delete.success'));
 		}
 	}
-	function readfile_chunked($filename,$retbytes=true) {
-   $chunksize = 1*(1024*1024); // how many bytes per chunk
-   $buffer = '';
-   $cnt =0;
-   // $handle = fopen($filename, 'rb');
-   $handle = fopen($filename, 'rb');
-   if ($handle === false) {
-   	return false;
-   }
-   while (!feof($handle)) {
-   	$buffer = fread($handle, $chunksize);
-   	echo $buffer;
-   	ob_flush();
-   	flush();
-   	if ($retbytes) {
-   		$cnt += strlen($buffer);
-   	}
-   }
-   $status = fclose($handle);
-   if ($retbytes && $status) {
-       return $cnt; // return num. bytes delivered like readfile() does.
-   }
-   return $status;
 
-}
-
-
-function rangeDownload($file){
-	$fp = @fopen($file, 'rb');
-
-        $size   = filesize($file); // File size
-        $length = $size;           // Content length
-        $start  = 0;               // Start byte
-        $end    = $size - 1;       // End byte
-        // Now that we've gotten so far without errors we send the accept range header
-        /* At the moment we only support single ranges.
-         * Multiple ranges requires some more work to ensure it works correctly
-         * and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-         *
-         * Multirange support annouces itself with:
-         * header('Accept-Ranges: bytes');
-         *
-         * Multirange content must be sent with multipart/byteranges mediatype,
-         * (mediatype = mimetype)
-         * as well as a boundry header to indicate the various chunks of data.
-         */
-        header("Accept-Ranges: 0-$length");
-        // header('Accept-Ranges: bytes');
-        // multipart/byteranges
-        // http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
-        if (isset($_SERVER['HTTP_RANGE'])){
-        	$c_start = $start;
-        	$c_end   = $end;
-
-            // Extract the range string
-        	list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-            // Make sure the client hasn't sent us a multibyte range
-        	if (strpos($range, ',') !== false){
-                // (?) Shoud this be issued here, or should the first
-                // range be used? Or should the header be ignored and
-                // we output the whole content?
-        		header('HTTP/1.1 416 Requested Range Not Satisfiable');
-        		header("Content-Range: bytes $start-$end/$size");
-                // (?) Echo some info to the client?
-        		exit;
-            } // fim do if
-            // If the range starts with an '-' we start from the beginning
-            // If not, we forward the file pointer
-            // And make sure to get the end byte if spesified
-            if ($range{0} == '-'){
-                // The n-number of the last bytes is requested
-            	$c_start = $size - substr($range, 1);
-            } else {
-            	$range  = explode('-', $range);
-            	$c_start = $range[0];
-            	$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
-            } // fim do if
-            /* Check the range and make sure it's treated according to the specs.
-             * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
-             */
-            // End bytes can not be larger than $end.
-            $c_end = ($c_end > $end) ? $end : $c_end;
-            // Validate the requested range and return an error if it's not correct.
-            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size){
-            	header('HTTP/1.1 416 Requested Range Not Satisfiable');
-            	header("Content-Range: bytes $start-$end/$size");
-                // (?) Echo some info to the client?
-            	exit;
-            } // fim do if
-
-            $start  = $c_start;
-            $end    = $c_end;
-            $length = $end - $start + 1; // Calculate new content length
-            fseek($fp, $start);
-            header('HTTP/1.1 206 Partial Content');
-        } // fim do if
-
-        // Notify the client the byte range we'll be outputting
-        header("Content-Range: bytes $start-$end/$size");
-        header("Content-Length: $length");
-
-        // Start buffered download
-        $buffer = 1024 * 8;
-        while(!feof($fp) && ($p = ftell($fp)) <= $end){
-        	if ($p + $buffer > $end){
-                // In case we're only outputtin a chunk, make sure we don't
-                // read past the length
-        		$buffer = $end - $p + 1;
-            } // fim do if
-
-            set_time_limit(0); // Reset time limit for big files
-            echo fread($fp, $buffer);
-            flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
-        } // fim do while
-
-        fclose($fp);
-    } // fim do function
-
-
-
-    public function file($alphaID)
-    {
+	public function file($alphaID)
+	{
 		// echo '?good permissions?<br>';
 
 
-    	$asset = Asset::where('alphaID', '=', $alphaID)->firstOrFail();
-    	$path = Config::get('settings.media-path');
-    	$ext = 'mp4';
-    	$file = base_path(). "/" . $path . "/" . $alphaID.  '.' . $ext;
-    	$filesize = filesize($file);
-    	$mime = Mimes::getMimes($ext);
+		$asset = Asset::where('alphaID', '=', $alphaID)->firstOrFail();
+		$path = Config::get('settings.media-path');
+		$ext = $asset->original_ext;
+		$file = base_path(). "/" . $path . "/" . $alphaID.  '.' . $ext;
+		$mime = Mimes::getMimes($ext);
 
 
 
-    	if (is_file($file)){
-    		header('Content-Type:'. $mime);
+		if(file_exists($file)){
+			$filesize = filesize($file);
+		}
+
+
+
+
+		if (is_file($file)){
+			header('Content-Type:'. $mime);
 			if (isset($_SERVER['HTTP_RANGE'])){ // do it for any device that supports byte-ranges not only iPhone
 				$this->rangeDownload($file);
 			} else {
@@ -358,105 +245,67 @@ function rangeDownload($file){
 				readfile($file);
 			}
 		}
-
-
-
-		// try {
-		// 	$asset = Asset::where('alphaID', '=', $alphaID)->firstOrFail();
-		// }
-		// catch (ModelNotFoundException $e) {
-		// 	return Response::make('Sorry, The file: '.$alphaID.' doesn\'t seem to exsist', 404);
-		// }
-
-		// if ($item == "original" && $asset->type == "video" || $item == "original" && $asset->type == "audio") {
-			// $path = Config::get('settings.media-path-original');
-		// }
-		// else{
-		// 	$path = Config::get('settings.media-path');
-		// }
-
-		// if($item == "original"){
-		// 	$ext = $asset->original_ext;
-		// }
-		// else{
-		// 	switch ($asset->type) {
-		// 		case 'video':
-		// 			$ext = 'mp4';
-		// 			break;
-		// 		case 'audio':
-		// 			$ext = 'mp3';
-		// 			break;
-		// 		default:
-		// 			$ext = $asset->original_ext;
-		// 			break;
-		// 	}
-		// }
-
-		// switch ($item) {
-		// 	case 'thumb':
-		// 	$file = base_path(). "/" . $path . "/" . $alphaID.  "-thumb.jpg";
-		// 	$ext = 'jpg';
-		// 	break;
-
-		// 	default:
-		// 		$file = base_path(). "/" . $path . "/" . $alphaID.  '.' . $ext;
-		// 	break;
-		// }
-
-		// // echo $file .  ' - - - -  ' . Mimes::getMimes($ext) . ' - - - -  ' .filesize($file) . '  --- ';
-		// // die();
-
-		// $fm=@fopen($file,'rb');
-		// if(!$fm) {
-		// 	// You can also redirect here
-		// 	header ("HTTP/1.0 404 Not Found");
-		// 	die();
-		// }
-
-		// $size=filesize($file);
-
-		// $begin=0;
-		// $end=$size;
-
-
-		// if(isset($_SERVER['HTTP_RANGE'])) {
-		// 	if(preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
-		// 		$begin=intval($matches[0]);
-		// 		if(!empty($matches[1])) {
-		// 			$end=intval($matches[1]);
-		// 		}
-		// 	}
-		// }
-
-		// if($begin>0||$end<$size)
-		// 	header('HTTP/1.0 206 Partial Content');
-		// else
-		// 	header('HTTP/1.0 200 OK');
-
-		// header('Content-Type:'. Mimes::getMimes($ext));
-		// header('Accept-Ranges: bytes');
-		// header('Content-Length:'.($end-$begin));
-		// header("Content-Disposition: inline;");
-		// header("Content-Range: bytes $begin-$end/$size");
-		// header("Content-Transfer-Encoding: binary\n");
-		// header('Connection: close');
-
-		// $cur=$begin;
-		// fseek($fm,$begin,0);
-
-		// while(!feof($fm)&&$cur<$end&&(connection_status()==0))
-		// 	{ print fread($fm,min(1024*16,$end-$cur));
-		// 		$cur+=1024*16;
-		// 		usleep(1000);
-		// 	}
-		// die();
-
-
-
-
-
 	}
 
+	function rangeDownload($file){
+		$fp = @fopen($file, 'rb');
 
+        $size   = filesize($file);
+        $length = $size;
+        $start  = 0;
+        $end    = $size - 1;
+
+
+        header("Accept-Ranges: 0-$length");
+
+
+        if (isset($_SERVER['HTTP_RANGE'])){
+        	$c_start = $start;
+        	$c_end   = $end;
+        	list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+        	if (strpos($range, ',') !== false){
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+        		header("Content-Range: bytes $start-$end/$size");
+        		exit;
+            }
+
+            if ($range{0} == '-'){
+            	$c_start = $size - substr($range, 1);
+            } else {
+            	$range  = explode('-', $range);
+            	$c_start = $range[0];
+            	$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+            }
+
+            $c_end = ($c_end > $end) ? $end : $c_end;
+
+            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size){
+            	header('HTTP/1.1 416 Requested Range Not Satisfiable');
+            	header("Content-Range: bytes $start-$end/$size");
+            	exit;
+            }
+
+            $start  = $c_start;
+            $end    = $c_end;
+            $length = $end - $start + 1; 
+            fseek($fp, $start);
+            header('HTTP/1.1 206 Partial Content');
+
+        }
+        header("Content-Range: bytes $start-$end/$size");
+        header("Content-Length: $length");
+
+        $buffer = 1024 * 8;
+        while(!feof($fp) && ($p = ftell($fp)) <= $end){
+        	if ($p + $buffer > $end){
+        		$buffer = $end - $p + 1;
+            }
+            set_time_limit(0);
+            echo fread($fp, $buffer);
+            flush(); 
+        }
+
+        fclose($fp);
+    }
 
 }
