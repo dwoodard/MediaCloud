b0e8218345ed6faaafcfdfebbdaf70f6786a6edc
Dustin Woodard <dustinwoodard@weber.edu>
2014-03-04 17:17:41 -0700

    added angular files

--- app/views/frontend/manage/index.blade.php
+++ app/views/frontend/manage/index.blade.php
@@ -1,12 +1,15 @@
 @extends('frontend/layouts/default')
 
 @section('content')
+<div id="App" ng-app="mcApp">
 
 
-<div id="App">
-	test
+
 </div>
+@stop
 
 
-
+@section('scripts')
+ <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js"></script>
+ <script src="/app/app.js"></script>
 @stop
