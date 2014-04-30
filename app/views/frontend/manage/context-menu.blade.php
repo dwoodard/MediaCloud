1d99150d6ad85bd97bcf1ecfc4c08ef2f3161127
Dustin Woodard <dustinwoodard@weber.edu>
2014-04-30 13:30:12 -0600

    managejs delete working good

--- app/views/frontend/manage/context-menu.blade.php
+++ app/views/frontend/manage/context-menu.blade.php
@@ -3,33 +3,41 @@
 <link href="/assets/css/manage.css" rel="stylesheet">
 <script src="/bower/jquery/dist/jquery.min.js"></script>
 <script src="/bower/bootstrap/dist/js/bootstrap.min.js"></script>
- -->
+-->
 
 <?php $key=uniqid(); ?>
-	
+
 <div id="context-menu-{{$key}}" class="context-menu carousel slide">
 	<div class="carousel-inner">
 		<div class="item active">
 			<ul id="menu-collection">
-				<li class="slide-submenu">
-					<a href="#" id="share" data-target="#context-menu-{{$key}}" data-slide-to="3">Share...</a>
-				</li>
 				<li><a href="#" id="play">Play</a></li>
-<!-- 				<li class="slide-submenu">
-					<a href="#" id="rename" data-target="#context-menu-{{$key}}" data-slide-to="2"> Rename </a>
-				</li>
- -->				<li class="slide-submenu">
-					<a href="#" id="add-to" data-target="#context-menu-{{$key}}" data-slide-to="1">
+				<li class="slide-submenu">
+					<a href="#" id="add-to" data-target="#context-menu-{{$key}}" data-slide-to="2">
 						Add to...
 					</a>
 				</li>
 				<li><a href="#" id="publish">Make public</a></li>
-				<li><a href="#" id="copy-url">Copy URL<span></span></a></li>
 				<li class="slide-submenu">
-					<a href="#" data-target="#context-menu-{{$key}}" data-slide-to="4">Remove...</a>
+					<a href="#" data-target="#context-menu-{{$key}}" data-slide-to="1">Remove...</a>
 				</li>
 			</ul>
 		</div>
+
+
+		<div class="item">
+			<header>
+				<a class="back" data-target="#context-menu-{{$key}}" data-slide-to="0"> <i class="fa fa-arrow-circle-o-left"></i> Back</a>
+			</header>
+
+			<div>
+				<p>This will not delete the file(s) completely, but assets will be removed from collections or playlist</p>
+				<p>
+					<a href="#" id="delete-item" class="btn btn-default"> <i class="fa fa-trash-o"></i> Remove<span class="delete-check"></span></a>
+				</p>
+			</div>
+		</div>
+
 
 		<div class="item">
 			<header>
@@ -48,26 +56,20 @@
 			<div>Rename</div>
 		</div>
 
-		<div class="item">
-			<header>
-				<a class="back" data-target="#context-menu-{{$key}}" data-slide-to="0"> <i class="fa fa-arrow-circle-o-left"></i> Back</a>
-			</header>
-
-			<div>Share</div>
-		</div>
-
-		<div class="item">
+		<div id="slide-share" class="item">
 			<header>
 				<a class="back" data-target="#context-menu-{{$key}}" data-slide-to="0"> <i class="fa fa-arrow-circle-o-left"></i> Back</a>
 			</header>
 
 			<div>
-				<p>This will not delete the file(s) completely, but assets will be removed from collections or playlist</p>
-				<p>
-					<a href="#" id="delete-item" class="btn btn-default"> <i class="fa fa-trash-o"></i> Remove<span class="delete-check"></span></a>
-				</p>
+
+				<input  type="text" value="asdfasdf">
+
+
+
 			</div>
 		</div>
+
 
 	</div>
 </div>
