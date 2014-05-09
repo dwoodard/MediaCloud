b722eb7e1eba30f43f277c1ddaaabd14224d200a
Dustin Woodard <dustin.woodard@gmail.com>
2014-05-09 00:56:16 -0600

    added player.css

--- app/views/player/collection.blade.php
+++ app/views/player/collection.blade.php
@@ -6,14 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="/bower/bootstrap/dist/css/bootstrap.min.css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link href="/assets/css/player.css" rel="stylesheet">
 </head>
 <body>
 
-{{--@include('_partials/player-cpa'); --}}
-	<div id="mediaplayer-wrapper">
-		<div id="player-video-wrapper">
-			<video id="player-video">Your browser does not support the video tag.</video>
-		</div>
+	<nav id="player-menu-nav" class="push-menu-open">
 		<div id="player-menu">
 			<header id="menuTitle">menuTitle</header>
 			<ul id="playlists">
@@ -22,6 +19,14 @@
 			<ul id="assets">
 				assets
 			</ul>
+		</div>
+	</nav>
+
+
+	{{--@include('_partials/player-cpa'); --}}
+	<div id="mediaplayer-wrapper">
+		<div id="player-video-wrapper">
+			<video id="player-video">Your browser does not support the video tag.</video>
 		</div>
 	</div>
 
